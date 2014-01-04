c92415152b94cc9b5ad126e6f9371eb22837bd7f
uqwwan10 <uqwwan10@uq.edu.au>
2014-01-04 14:09:07 +1000

    init commit

---
+++ au.uq.dke.comon_rcp2/src/au/uq/dke/comon_rcp2/ApplicationWorkbenchAdvisor.java
@@ -0,0 +1,20 @@
+package au.uq.dke.comon_rcp2;
+
+import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
+import org.eclipse.ui.application.WorkbenchAdvisor;
+import org.eclipse.ui.application.WorkbenchWindowAdvisor;
+
+public class ApplicationWorkbenchAdvisor extends WorkbenchAdvisor {
+
+	private static final String PERSPECTIVE_ID = "au.uq.dke.comon_rcp2.perspective";
+
+	public WorkbenchWindowAdvisor createWorkbenchWindowAdvisor(
+			IWorkbenchWindowConfigurer configurer) {
+		return new ApplicationWorkbenchWindowAdvisor(configurer);
+	}
+
+	public String getInitialWindowPerspectiveId() {
+		return PERSPECTIVE_ID;
+	}
+
+}
