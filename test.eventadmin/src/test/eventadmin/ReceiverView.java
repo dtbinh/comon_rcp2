4edbaa266bf1efe908eef1216077e3531d0bf6b9
steve at home using ideapad <weiwang.thu@gmail.com>
2014-01-05 12:33:37 +1000

    RESOLVED - bug 46: test multi sender and receiver using eventadmin https://www.bugheaven.com/bugzilla/DKE/show_bug.cgi?id=46

---
+++ test.eventadmin/src/test/eventadmin/ReceiverView.java
@@ -0,0 +1,61 @@
+package test.eventadmin;
+
+import java.text.DateFormat;
+import java.util.Dictionary;
+import java.util.Hashtable;
+
+import org.eclipse.jface.viewers.ColumnLabelProvider;
+import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.part.ViewPart;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.FrameworkUtil;
+import org.osgi.service.event.Event;
+import org.osgi.service.event.EventConstants;
+import org.osgi.service.event.EventHandler;
+
+public class ReceiverView extends ViewPart {
+	private TableViewer viewer;
+
+	@Override
+	public void createPartControl(final Composite parent) {
+		parent.setLayout(new FillLayout());
+		viewer = new TableViewer(parent);
+		viewer.getTable().setHeaderVisible(true);
+		viewer.getTable().setLinesVisible(true);
+		viewer.setLabelProvider(new ColumnLabelProvider() {
+			@Override
+			public String getText(Object element) {
+				//return DateFormat.getDateTimeInstance().format(element);
+				return element.toString();
+			}
+		});
+
+		BundleContext ctx = FrameworkUtil.getBundle(ReceiverView.class)
+				.getBundleContext();
+		EventHandler handler = new EventHandler() {
+			public void handleEvent(final Event event) {
+				if (parent.getDisplay().getThread() == Thread.currentThread()) {
+					
+					viewer.add(event.getTopic() + ": " + event.getProperty("DATA"));
+				} else {
+					parent.getDisplay().syncExec(new Runnable() {
+						public void run() {
+							viewer.add(event.getProperty("DATA"));
+						}
+					});
+				}
+			}
+		};
+
+		Dictionary<String, String> properties = new Hashtable<String, String>();
+		properties.put(EventConstants.EVENT_TOPIC, "viewcommunication/*");
+		ctx.registerService(EventHandler.class, handler, properties);
+	}
+
+	@Override
+	public void setFocus() {
+		viewer.getTable().setFocus();
+	}
+}
---
+++ test.eventadmin/src/test/eventadmin/SenderView.java
@@ -0,0 +1,56 @@
+package test.eventadmin;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.part.ViewPart;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.FrameworkUtil;
+import org.osgi.framework.ServiceReference;
+import org.osgi.service.event.Event;
+import org.osgi.service.event.EventAdmin;
+
+public class SenderView extends ViewPart {
+
+	public static final String ID = "viewcommunication.views.SenderView";
+	private Button b;
+
+	public void createPartControl(Composite parent) {
+		parent.setLayout(new GridLayout());
+		b = new Button(parent, SWT.PUSH);
+		b.setText("Send Event");
+		b.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				BundleContext ctx = FrameworkUtil.getBundle(SenderView.class)
+						.getBundleContext();
+				ServiceReference<EventAdmin> ref = ctx
+						.getServiceReference(EventAdmin.class);
+				EventAdmin eventAdmin = ctx.getService(ref);
+				Map<String, Object> properties = new HashMap<String, Object>();
+				properties.put("DATA", new Date());
+
+				Event event = new Event("viewcommunication/syncEvent",
+						properties);
+				eventAdmin.sendEvent(event);
+
+				event = new Event("viewcommunication/asyncEvent", properties);
+//				eventAdmin.postEvent(event);
+			}
+		});
+	}
+
+	/**
+	 * Passing the focus request to the viewer's control.
+	 */
+	public void setFocus() {
+		b.setFocus();
+	}
+}
