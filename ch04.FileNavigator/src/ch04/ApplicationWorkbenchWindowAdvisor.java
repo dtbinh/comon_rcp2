7c11397f500720ebea635d2fd95276257afaefd6
steve at home using ideapad <weiwang.thu@gmail.com>
2014-01-05 23:00:37 +1000

    added the file navigator example

---
+++ ch04.FileNavigator/src/ch04/ApplicationWorkbenchWindowAdvisor.java
@@ -0,0 +1,36 @@
+package ch04;
+
+import org.eclipse.ui.application.ActionBarAdvisor;
+import org.eclipse.ui.application.IActionBarConfigurer;
+import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
+import org.eclipse.ui.application.WorkbenchWindowAdvisor;
+
+import ch04.jobs.Race;
+
+public class ApplicationWorkbenchWindowAdvisor extends
+        WorkbenchWindowAdvisor {
+
+    public ApplicationWorkbenchWindowAdvisor(
+            IWorkbenchWindowConfigurer configurer) {
+        super(configurer);
+    }
+
+    public ActionBarAdvisor createActionBarAdvisor(
+            IActionBarConfigurer configurer) {
+        return new ApplicationActionBarAdvisor(configurer);
+    }
+
+    public void preWindowOpen() {
+        IWorkbenchWindowConfigurer configurer = getWindowConfigurer();
+        configurer.setShowCoolBar(false);
+        configurer.setShowStatusLine(false);
+        configurer.setShowStatusLine(true);
+        configurer.setShowProgressIndicator(true);
+    }
+
+    @Override
+    public void postWindowOpen() {
+        super.postWindowOpen();
+        new Race().start();
+    }
+}
