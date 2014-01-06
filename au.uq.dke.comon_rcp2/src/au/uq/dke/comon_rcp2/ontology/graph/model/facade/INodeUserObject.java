8e84eff08ad97c619e8314337a764ab4a22ca48d
maxpayne200 <weiwang.thu@gmail.com>
2014-01-06 15:00:22 +1000

    RESOLVED - bug 51: implement private generate roots, branches, level. aka init information https://www.bugheaven.com/bugzilla/DKE/show_bug.cgi?id=51
    delete related methods in ontologyclass and the interface

--- au.uq.dke.comon_rcp2/src/au/uq/dke/comon_rcp2/ontology/graph/model/facade/INodeUserObject.java
+++ au.uq.dke.comon_rcp2/src/au/uq/dke/comon_rcp2/ontology/graph/model/facade/INodeUserObject.java
@@ -8,24 +8,5 @@ public interface INodeUserObject {
 
 	public String getText();
 
-	public boolean isRoot();
-
-	public void setRoot(boolean isRoot);
-
-	public boolean isBranch();
-
-	public void setBranch(boolean isBranch);
-
-	public boolean isHasChildren();
-
-	public void setHasChildren(boolean hasChildren);
-
-	public OntologyClass getBranchNode();
-
-	public void setBranchNode(OntologyClass branchNode);
-
-	public int getLevel();
-
-	public void setLevel(int level);
 
 }
--- au.uq.dke.comon_rcp2/src/au/uq/dke/comon_rcp2/ontology/model/OntologyClass.java
+++ au.uq.dke.comon_rcp2/src/au/uq/dke/comon_rcp2/ontology/model/OntologyClass.java
@@ -13,20 +13,6 @@ public class OntologyClass extends OntologyItem implements INodeUserObject{
 	
 	public String name = "hehe";
 	
-	public boolean isRoot = false;;
-	
-	public boolean isBranch = false;
-	
-	public boolean hasChildren = false;;
-	
-	public OntologyClass branchNode = null;
-	
-	public int level;
-	
-	public Set <OntologyClass> children = null;
-	
-	
-	
 	public OntologyClass(){
 		
 	}
@@ -48,56 +34,6 @@ public class OntologyClass extends OntologyItem implements INodeUserObject{
 	public String getText() {
 		// TODO Auto-generated method stub
 		return this.getName();
-	}
-
-	public boolean isRoot() {
-		return isRoot;
-	}
-
-	public void setRoot(boolean isRoot) {
-		this.isRoot = isRoot;
-	}
-
-	public boolean isBranch() {
-		return isBranch;
-	}
-
-	public void setBranch(boolean isBranch) {
-		this.isBranch = isBranch;
-	}
-
-	public boolean isHasChildren() {
-		return hasChildren;
-	}
-
-	public void setHasChildren(boolean hasChildren) {
-		this.hasChildren = hasChildren;
-	}
-
-	@Transient
-	public OntologyClass getBranchNode() {
-		return branchNode;
-	}
-
-	public void setBranchNode(OntologyClass branchNode) {
-		this.branchNode = branchNode;
-	}
-
-	public int getLevel() {
-		return level;
-	}
-
-	public void setLevel(int level) {
-		this.level = level;
-	}
-
-	@Transient
-	public Set<OntologyClass> getChildren() {
-		return children;
-	}
-
-	public void setChildren(Set<OntologyClass> children) {
-		this.children = children;
 	}
 
 }
