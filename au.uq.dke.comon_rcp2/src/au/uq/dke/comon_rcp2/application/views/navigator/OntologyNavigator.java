2370109a62d44a967e80f2c60f466825a37ca2a8
maxpayne200 <weiwang.thu@gmail.com>
2014-01-06 17:40:12 +1000

    RESOLVED - bug 54: study the filter https://www.bugheaven.com/bugzilla/DKE/show_bug.cgi?id=54

--- au.uq.dke.comon_rcp2/src/au/uq/dke/comon_rcp2/application/views/navigator/OntologyNavigator.java
+++ au.uq.dke.comon_rcp2/src/au/uq/dke/comon_rcp2/application/views/navigator/OntologyNavigator.java
@@ -1,14 +1,25 @@
 package au.uq.dke.comon_rcp2.application.views.navigator;
 
+import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.navigator.CommonNavigator;
 
+import au.uq.dke.comon_rcp2.application.views.navigator.filters.TestViewerFilter;
 import au.uq.dke.comon_rcp2.application.views.navigator.model.NavigatorRoot;
 
 
 public class OntologyNavigator extends CommonNavigator {
+
 	NavigatorRoot navigatorRoot = new NavigatorRoot();
 	
 	
+
+	@Override
+	public void createPartControl(Composite aParent) {
+		super.createPartControl(aParent);
+		
+		TestViewerFilter testViewerFilter = new TestViewerFilter();
+		super.getCommonViewer().addFilter(testViewerFilter);
+	}
 
 	public OntologyNavigator() {
 		// TODO Auto-generated constructor stub
---
+++ au.uq.dke.comon_rcp2/src/au/uq/dke/comon_rcp2/application/views/navigator/filters/TestViewerFilter.java
@@ -0,0 +1,20 @@
+package au.uq.dke.comon_rcp2.application.views.navigator.filters;
+
+import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.jface.viewers.ViewerFilter;
+
+import au.uq.dke.comon_rcp2.ontology.graph.model.node.BasicGraphNode;
+
+
+public class TestViewerFilter extends ViewerFilter {
+
+
+  @Override
+  public boolean select(Viewer viewer, 
+      Object parentElement, 
+      Object element) {
+    BasicGraphNode graphNode = (BasicGraphNode) element;
+
+    return true;
+  }
+} 
