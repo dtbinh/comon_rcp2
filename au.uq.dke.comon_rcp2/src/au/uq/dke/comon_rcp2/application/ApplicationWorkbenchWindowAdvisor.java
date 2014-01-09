b203629f8a4a7ed3c5551c92792a3c1245375e45
maxpayne200 <weiwang.thu@gmail.com>
2014-01-09 17:01:42 +1000

    RESOLVED - bug 114: perform a layout after ui shows https://www.bugheaven.com/bugzilla/DKE/show_bug.cgi?id=114
    workbenchWindowAdvisor
    	@Override
    	public void postWindowOpen() {
    		OntologyGraph.getInstance().performLayoutWithoutFilter();
    		super.postWindowOpen();
    	}

--- au.uq.dke.comon_rcp2/src/au/uq/dke/comon_rcp2/application/ApplicationWorkbenchWindowAdvisor.java
+++ au.uq.dke.comon_rcp2/src/au/uq/dke/comon_rcp2/application/ApplicationWorkbenchWindowAdvisor.java
@@ -6,6 +6,8 @@ import org.eclipse.ui.application.IActionBarConfigurer;
 import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
 import org.eclipse.ui.application.WorkbenchWindowAdvisor;
 
+import au.uq.dke.comon_rcp2.application.views.graph.OntologyGraph;
+
 public class ApplicationWorkbenchWindowAdvisor extends WorkbenchWindowAdvisor {
 
 	public ApplicationWorkbenchWindowAdvisor(IWorkbenchWindowConfigurer configurer) {
@@ -22,5 +24,17 @@ public class ApplicationWorkbenchWindowAdvisor extends WorkbenchWindowAdvisor {
 		configurer.setInitialSize(new Point(400, 300));
 		configurer.setShowCoolBar(false);
 		configurer.setShowStatusLine(false);
+	}
+
+	@Override
+	public void postWindowCreate() {
+		// TODO Auto-generated method stub
+		super.postWindowCreate();
+	}
+
+	@Override
+	public void postWindowOpen() {
+		OntologyGraph.getInstance().performLayoutWithoutFilter();
+		super.postWindowOpen();
 	}
 }
